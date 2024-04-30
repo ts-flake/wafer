adc3568fa31bb5d7093e35fba020dd110d61b300
ts-flake <ydh2@icloud.com>
2024-04-30 23:17:22 +0800

    remove dependency of fastai

---
+++ wafer/imports.py
@@ -0,0 +1,21 @@
+import torch, torcheval
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.utils.data import DataLoader
+
+from datasets import load_dataset, Dataset
+
+import numpy as np
+import pandas as pd
+from PIL import Image
+from matplotlib import animation
+import matplotlib.pyplot as plt
+
+import time
+from tqdm.notebook import tqdm
+from IPython.display import display, clear_output, HTML
+
+def noop (x=None, *args, **kwargs):
+    "Do nothing"
+    return x
